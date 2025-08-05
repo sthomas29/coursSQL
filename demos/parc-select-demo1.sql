af5ce09dd88318fc3e0b5c1c4d765f0a9e2f444f
Stéphane THOMAS <thomas91@free.fr>
2025-08-05 11:38:30 -0300

    MàJ fichier de démo SELECT - Jour 1 jusqu'au domaine de valeurs

--- demos/parc-select-demo1.sql
+++ demos/parc-select-demo1.sql
@@ -1,4 +1,20 @@
 use parc
 go
 
-SELECT * from attractions;
+SELECT * from attractions where taille_minimale_requise <= 130;
+SELECT * from attractions where taille_minimale_requise <= 130 AND capacite >= 25;
+
+SELECT * from attractions where capacite >= 25;
+
+SELECT * from attractions where type LIKE 'Sensations%';
+
+SELECT * from visiteurs where id=20;
+
+SELECT * from visiteurs  where taille > 130  AND age < 18;
+
+SELECT * from visiteurs where prenom IN ('Lucie', 'Hugues', 'Hugo', 'Mathis', 'Sophie');
+
+SELECT * from visiteurs where prenom like '%Lucie%'
+						OR prenom like '%Hugo%'
+						OR prenom like '%Mathis%'
+						OR prenom like '%Sophie%';
