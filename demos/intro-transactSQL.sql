3edb111fed9292fd29cebaacb8d52b58a5c546c2
Stéphane <sthomas@campus-eni.fr>
2025-08-07 16:20:35 +0200

    Update démo-transactSQL

--- demos/intro-transactSQL.sql
+++ demos/intro-transactSQL.sql
@@ -26,23 +26,24 @@ print concat('0-Affichage via d''une variable : ', @nb_attractions);
 declare @resultat varchar(50) = CONCAT('1-Nb d''attractions : ', @nb_attractions);
 print @resultat;
 
-print concat('2-Nb d''attractions : ',  @nb_attractions)
+print concat('2-Nb d''attractions : ',  @nb_attractions);
 --print @nb_attractions;
 
 /********** Les conditions avec Transact *****/
 -- Affectation de la valeur 19 ici pour tester le bloc ELSE
 -- à commenter pour tester le bloc IF
-select @nb_attractions=19
+select @nb_attractions=19;
 
 -- Bloc conditionnel selon une valeur de variable
 if @nb_attractions = 20
 -- Les instructions BEGIN... END servent à définir un bloc de code, impératif
 -- pour les conditions et les boucles.
 begin
-	print concat('Il y a bien ',@nb_attractions, ' attractions dans le parc')
+	print concat('Il y a bien ',@nb_attractions, ' attractions dans le parc');
 end
 else
 begin
-	print concat('Il n''y a plus 20 attractions, il y en a ',@nb_attractions, ' dans le parc')
+	print concat('Il n''y a plus 20 attractions, il y en a ',@nb_attractions, ' dans le parc');
 end
+
 
